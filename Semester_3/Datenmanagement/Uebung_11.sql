f6c8210038a6ab616956b05e6ac1082a73d9e265
Maltrix147 <malte.hermann@gmx.net>
2025-01-21 00:14:28 +0100

    sql

--- Semester_3/Datenmanagement/Uebung_11.sql
+++ Semester_3/Datenmanagement/Uebung_11.sql
@@ -3,3 +3,16 @@ nachname, vorname, datum
 From
 bestellung inner join kunde on kunde.kunde_id = bestellung.kunde_id
 order by nachname; datum desc
+
+SELECT bestellung.kunde_id, bestellung.datum, positionen.position_id 
+FROM bestellung 
+INNER JOIN positionen 
+ON bestellung.bestell_id = positionen.bestell_id 
+ORDER BY bestellung.kunde_id, bestellung.bestell_id, positionen.position_id;
+
+SELECT position_nr, bezeichnung 
+FROM artikel a
+INNER JOIN bestellung_position bp
+ON positionen.artikel_id = artikel.artikel_id 
+ORDER BY artikel.bezeichnung;
+
