2ba89c77a05b73b840f1bcec79ec757851a2d6a3
Maltrix147 <malte.hermann@gmx.net>
2025-06-16 22:49:19 +0200

    3d_animatic

---
+++ Semester_4/Software_Engineering/VS_22_Repos/Auto/Auto/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SomeCars
+{
+    class Auto
+    {
+        string fahrzeugFarbe;
+
+        public string Farbe
+        {
+            get { return fahrzeugFarbe; }
+            set { fahrzeugFarbe = value; }
+        }
+        int fahrzeugBaujahr;
+
+        public int Baujahr
+        {
+            get { return fahrzeugBaujahr; }
+            set { fahrzeugBaujahr = value; }
+        }
+
+        public Auto(string farbe, int baujahr)
+        {
+            this.Farbe = farbe;
+            this.Baujahr = baujahr;
+        }
+    }
+
+    class PKW : Auto
+    {
+        private int sitze;
+        public int Sitzplätze
+        {
+            get { return Sitzplätze; }
+            set { Sitzplätze = value; }
+        }
+        public Auto{int Baujahr
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto Auto1 = new Auto("Rot", 1978);
+            Auto Auto2 = new Auto("Gelb", 2011);
+
+            Console.WriteLine("Auto1: {0}\t{1}", Auto1.Farbe, Auto1.Baujahr);
+            Console.WriteLine("Auto2: {0}\t{1}", Auto2.Farbe, Auto2.Baujahr);
+
+            Auto1.Farbe = "Gruen";
+            Auto2.Farbe = "Blau";
+
+            Console.WriteLine("\nAuto1: {0}\t{1}", Auto1.Farbe, Auto1.Baujahr);
+            Console.WriteLine("Auto2: {0}\t{1}", Auto2.Farbe, Auto2.Baujahr);
+        }
+    }
+}
---
+++ Semester_4/Software_Engineering/VS_22_Repos/Lampen/Lampen/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Lampen
+{
+    class Lamp
+    {
+        public int watt;
+
+        public Lamp(int watt)
+        {
+            this.watt = watt;
+        }
+
+        public int getWatt()
+        {
+            return this.watt;
+        }
+
+        public int annualPowerConsumption(int hoursperday)
+        {
+            int consumption = (this.watt * hoursperday * 365) / 1000;
+            return consumption;
+        }
+
+    }
+    class Bulb : Lamp
+    {
+        public Bulb(int watt) : base(watt)
+        {
+        }
+
+        public override string ToString()
+        {
+                string ausgabe = $"A bulb consumes {annualPowerConsumption(10)}kWh per year";
+                return ausgabe;
+        }
+    }
+
+    class LEDBulb : Lamp
+    {
+        public LEDBulb(int watt) : base(watt) { }
+        public override string ToString()
+        {
+            string ausgabe = $"A bulb consumes {annualPowerConsumption(10)}kWh per year";
+            return ausgabe;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bulb bulb = new Bulb(60);
+            LEDBulb bulb2 = new LEDBulb(9);
+
+            Console.WriteLine(bulb);
+            Console.WriteLine(bulb2);
+        }
+    }
+}
